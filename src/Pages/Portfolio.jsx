27e9232fdb53662594b0f57d8a823c92a5f0dbfa
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2023-07-14 10:43:34 +0630

    porfo title

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import RootLayout from '../layout/RootLayout'
+import React from "react";
+import RootLayout from "../layout/RootLayout";
 
 const Portfolio = () => {
   return (
     <RootLayout>
-      Portfolio
+      <div className="">
+        <div className=" container mx-[100px] py-[90px]">
+          <h3 className=" text-4xl tracking-tight uppercase font-semibold relative inline-block align-top z-10 mb-[10px] py-[10px] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:bg-[#d9832e] after:w-[50px]">
+            Latest Works
+          </h3>
+          <p className=" text-slate-600 tracking-wider text-sm">A JUNIOR WEB DEVELOPER BASED IN MYANMAR</p>
+        </div>
+      </div>
     </RootLayout>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
