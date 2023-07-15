01edeb6e0f8f758465bc9ed639c9722aca3bdb40
james marcus <thirdgodiswinning@gmail.com>
2023-07-15 13:34:57 +0630

    blog finished

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import RootLayout from '../layout/RootLayout'
+import React from "react";
+import RootLayout from "../layout/RootLayout";
 
 const Contact = () => {
   return (
     <RootLayout>
-      contact
+      <div className=" p-8 py-[100px] bg-slate-200">
+        <h2 className="text-4xl font-semibold mb-8">Latest Blogs</h2>
+        <h3 className="mb-14">A LEAD UX & UI DESIGNER BASED IN CANADA</h3>
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="">
+            <input
+              type="text"
+              placeholder="Name *"
+              className=" bg-slate-200 outline-none  py-2 border-b border-slate-600"
+            />
+          </div>
+          <div className="">
+            <div className=""> box</div>
+            <div className=""> box</div>
+            <div className=""> box</div>
+          </div>
+        </div>
+      </div>
     </RootLayout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
