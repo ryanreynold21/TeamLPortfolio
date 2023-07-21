ce7c6c4a2c904839b1075304c7119644d4e38bf3
Lenovo <aunkyawin2035@gmail.com>
2023-07-21 12:00:14 +0630

    knkkUpdate

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation'
 import { BsChevronDown } from 'react-icons/bs'
-import home from '../images/home-banner.jpg'
+import mee from '../images/mee.png'
 import { Link } from 'react-scroll'
 
 const Home = () => {
@@ -24,8 +24,8 @@ const Home = () => {
                 <h1 className=" text-[#090A23] text-[16px] md:text-[18px] lg:text-[20px] font-[500] tracking-wider mt-16">
                   Hello there...
                 </h1>
-                <h1 className=" text-[#090A23] text-[40px] md:text-[60px] lg:text-[70px] font-[600]">
-                  Tony Smith
+                <h1 className=" text-[#090A23] text-[30px] md:text-[60px] lg:text-[70px] font-[600]">
+                  Khant Nyar Ko Ko
                 </h1>
                 <div className=" flex items-center mb-5 text-[20px] md:text-[23px] lg:text-[25px]">
                   <h1 className=" font-[500] mr-2">I Am Passionate </h1>
@@ -66,8 +66,8 @@ const Home = () => {
             {/* image */}
             <div className=" hidden lg:block ">
               <img
-                src={home}
-                className="object-none object-right xl:w-screen lg:h-screen"
+                src={mee}
+                className="object-none object-right xl:w-screen lg:h-[1000px]"
                 alt=""
               />
             </div>
